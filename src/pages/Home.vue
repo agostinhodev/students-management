917800c2e90eb44fc72d4f17508f120ac5330ce3
agostinhodev <agostinhonetodev@gmail.com>
2020-11-29 10:56:50 -0300

    Added method to show Welcome message to our api

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,57 @@
 <template>
-  <div></div>
+  <div>
+
+    <b-container>
+      <b-row class="vh-100 text-center" align-v="center">
+        <b-col>
+          
+          {{message}}
+
+        </b-col>
+      </b-row>
+    </b-container>
+
+  </div>
 </template>
 <script>
 
+  import api from "@/services/api";
 
 export default {
+
+
   name: "Home",
-  components: {
-   
+  
+  data(){
+
+    return {
+
+      message: "Loading..."
+
+    };
+
+  },
+
+  created(){
+
+    this.showHomeMessage();
+
+  },
+
+  methods:{
+
+    showHomeMessage(){
+
+      api.get('')
+      .then(( response )=>{
+
+          const {data} = response;
+          this.message = data;
+
+      });
+
+    }
+
   }
 };
 </script>
